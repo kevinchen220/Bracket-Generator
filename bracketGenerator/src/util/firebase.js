fdfdf7d855533381f292066c8dabc16bbf826a70
kevinchen220 <68040593+kevinchen220@users.noreply.github.com>
2022-04-28 21:27:06 -0400

    remove firebase config

--- bracketGenerator/src/util/firebase.js
+++ bracketGenerator/src/util/firebase.js
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAb-YViQehXIPSl1aAMx9kjeCdz3CdR5iA",
-  authDomain: "bracketgenerator-220.firebaseapp.com",
-  projectId: "bracketgenerator-220",
-  storageBucket: "bracketgenerator-220.appspot.com",
-  messagingSenderId: "785432993769",
-  appId: "1:785432993769:web:683845fff0309374367578",
-  measurementId: "G-EQ43471J6S"
+  
 };
 
 // Initialize Firebase
